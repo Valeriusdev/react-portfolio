1a1ea7ebb0d5db7ca7e874800008c1b0b971e375
Valeriusdev <kakao757@gmail.com>
2023-08-29 20:37:19 -0700

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.css';
+import styled from 'styled-components';
+import {Header} from './layout/header/Header';
 
 function App() {
   return (
     <div className="App">
-
+      <Header />
     </div>
   );
 }
